a44573a159d70098c01cc7599c28a4bd15f0a984
FireSmallPanda <1020529941@qq.com>
2017-10-17 23:37:56 +0800

    提交前端代码

--- web-master/src/components/index/backstage/Backstage.vue
+++ web-master/src/components/index/backstage/Backstage.vue
@@ -1,21 +1,49 @@
 <template>
-    <div>
-      <div>Top</div>
-      <div>mid</div>
-      <div>foot</div>
+  <div id="back-stage">
+    <div class="back-stage-top">Top</div>
+    <div class="back-stage-mid">
+      <div class="row">
+        <div class="col-xs-4">
+          4
+        </div>
+        <div class="col-xs-8">
+          8
+        </div>
+      </div>
+      <div class="back-stage-foot">foot</div>
     </div>
+  </div>
 </template>
 
 <script>
-    export default{
-        data () {
-            return {
-                msg: ''
-            }
+  import cookieUtil from "@/common/js/cookieUtil"
+  export default{
+    data () {
+      return {
+        msg: '',
+        loginUser:{}
+
+      }
+    },
+    methods:{
+        // 检查登录
+        checkLogin(){
+          // 获取登录信息
+          this.loginUser = cookieUtil.get("loginUser");
+          if(!this.loginUser){
+            this.$router.push('/');//跳转登录
+          }
         }
+    },
+    created(){
+        this.checkLogin();
+
     }
+  }
 </script>
 
 <style scoped>
-
+  .back-stage {
+    height: 100%;
+  }
 </style>
