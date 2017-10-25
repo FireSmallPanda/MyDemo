271450ad6c58c3c9655bd234afa3be344046f171
FireSmallPanda <1020529941@qq.com>
2017-10-25 23:30:49 +0800

    添加redis的测试应用

---
+++ web-master/src/components/index/backstage/loginOut/LoginOut.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="loginOut">
+    登出
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: "",
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
